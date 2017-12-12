42f7535037f99bc9138dd0e4d6ca6fe34898774b
hyunjinLim <limhjko5239@gmail.com>
2017-12-12 20:22:54 +0900

    hello

---
+++ MDK-ARM/RTE/_ExamUart/RTE_Components.h
@@ -0,0 +1,28 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ExamUart' 
+ * Target:  'ExamUart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_UART
+
+#endif /* RTE_COMPONENTS_H */
